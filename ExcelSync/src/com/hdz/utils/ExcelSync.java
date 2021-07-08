1ac2c77e07267dc26b980344e62f3ce0db1bd82b
Your Name <cshdzxjtu@163.com>
2021-07-09 01:14:41 +0800

    代码提交

--- ExcelSync/src/com/hdz/utils/ExcelSync.java
+++ ExcelSync/src/com/hdz/utils/ExcelSync.java
@@ -1,24 +1,36 @@
 package com.hdz.utils;
 
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.util.IOUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ExcelSync {
     private String mSourceFile;
     private String mTargetFile;
+    private int mRefCol;
+    private String mRefValue;
+    private int mFillFromCol;
+    private int mKeyCol;
     private static final String CONFIG = "init.cfg";
     private static final String SEPERATOR = "\\";
+    private Map<String, ArrayList<String>> mFillContents = new HashMap<>();
 
     public ExcelSync(){
         loadConfig();
     }
 
     public void syncContents() {
+        pickFillContents();
+        syncToTarget();
+    }
+
+    private void pickFillContents() {
+        System.out.println("picking fill contents...");
         Workbook srcWorkbook = getWorkbook(mSourceFile);
         if (srcWorkbook == null) {
             System.out.println("source file error");
@@ -27,16 +39,73 @@ public class ExcelSync {
         Sheet sheet = srcWorkbook.getSheetAt(0);
         if (sheet != null) {
             int rowNum = sheet.getLastRowNum();
-            for (int i = 0; i <= rowNum; i++) {
+            for (int i = 1; i <= rowNum; i++) {
                 Row row = sheet.getRow(i);
                 int colNum = row.getLastCellNum();
-                for (int j = 0; j <= colNum; j++) {
-                    Cell cell = row.getCell(j);
-                    if (cell != null) {
-                        System.out.println(cell.getStringCellValue());
+
+                Cell refCell = row.getCell(mRefCol);
+                if (refCell == null) continue;
+                if (refCell.getCellType() != CellType.STRING) continue;
+                if (mRefValue.equals(refCell.getStringCellValue())) {
+                    int fillCol = mFillFromCol;
+                    ArrayList<String> lineContent = new ArrayList<>();
+                    for (; fillCol < colNum; fillCol++) {
+                        Cell pickCell = row.getCell(fillCol);
+                        if (pickCell == null) {
+                            lineContent.add(null);
+                        } else {
+                            CellType type = pickCell.getCellType();
+                            if (type == CellType.STRING) {
+                                lineContent.add(pickCell.getStringCellValue());
+                            } else if (type == CellType.NUMERIC) {
+                                lineContent.add(String.valueOf(pickCell.getNumericCellValue()));
+                            }
+                        }
+                    }
+                    if (!lineContent.isEmpty()) {
+                        Cell keyCell = row.getCell(mKeyCol);
+                        mFillContents.put(keyCell.getStringCellValue(), lineContent);
                     }
                 }
             }
+        }
+    }
+
+    private void syncToTarget() {
+        System.out.println("sync to target...");
+        try (FileOutputStream fos = new FileOutputStream(new File(mTargetFile))){
+            FileInputStream fis = new FileInputStream(new File(mTargetFile));
+            Workbook target = WorkbookFactory.create(fis);
+            if (target == null) return;
+            Sheet sheet = target.getSheetAt(0);
+            if (sheet == null) return;
+            int rowNum = sheet.getLastRowNum();
+
+            for (int i = 1; i <= rowNum; i++) {
+                Row row = sheet.getRow(i);
+
+                Cell keyCell = row.getCell(mKeyCol);
+                if (keyCell == null) continue;
+                if (keyCell.getCellType() != CellType.STRING) continue;
+
+                if (mFillContents.containsKey(keyCell.getStringCellValue())) {
+                    ArrayList<String> lineContent = mFillContents.get(keyCell.getStringCellValue());
+                    for (int j=0; j < lineContent.size(); j++) {
+                        int fillCol = mFillFromCol+j;
+                        Cell fillCell = row.getCell(fillCol);
+                        if (fillCell == null) {
+                            fillCell = row.createCell(fillCol);
+                        }
+                        String fillContent = lineContent.get(j);
+                        if (fillContent == null) continue;
+                        fillCell.setCellValue(fillContent);
+                    }
+                }
+            }
+            IOUtils.closeQuietly(fis);
+            target.write(fos);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -47,16 +116,35 @@ public class ExcelSync {
         System.out.println("working file is " + configFile);
         try {
             Scanner scanner = new Scanner(configFile);
+            int lineNum = 0;
             while (scanner.hasNext()) {
                 String config = scanner.nextLine();
+                lineNum++;
+                if (config.isEmpty() || config.startsWith("#")) continue;
                 String[] pair = config.split("=");
+                if (pair.length != 2) {
+                    System.out.println("invalid config at line#" + lineNum);
+                    continue;
+                }
                 String key = pair[0].trim();
                 String value = pair[1].trim();
+                if (value.isEmpty()) {
+                    System.out.println("invalid config at line#" + lineNum);
+                    continue;
+                }
                 if (key.equals("src")) {
                     mSourceFile = workingDir + value;
                     System.out.println(mSourceFile);
                 } else if (key.equals("target")) {
                     mTargetFile = workingDir + value;
+                } else if (key.equals("key")) {
+                    mKeyCol = value.charAt(0) - 'A';
+                } else if (key.equals("refCol")) {
+                    mRefCol = value.charAt(0) - 'A';
+                } else if (key.equals("refValue")) {
+                    mRefValue = value;
+                } else if (key.equals("fillStart")) {
+                    mFillFromCol = value.charAt(0) - 'A';
                 }
             }
         } catch (FileNotFoundException e) {
@@ -76,8 +164,6 @@ public class ExcelSync {
     }
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("hello world");
         new ExcelSync().syncContents();
     }
 }
